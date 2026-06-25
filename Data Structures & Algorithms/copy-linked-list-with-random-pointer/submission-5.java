56c4a5f9980210ff7174b7517756eabbcc9f945c
mo6044-hash <mo6044@princeton.edu>
2026-06-25 11:54:02 -0400

    Add: copy-linked-list-with-random-pointer - submission-5

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-5.java
@@ -0,0 +1,40 @@
+
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        HashMap<Node, Node> map = new HashMap<>();
+        Node r = new Node(0);
+        Node current = head;
+        while (current != null) {
+            Node copy = new Node(current.val);
+            map.put(current, copy);
+            current = current.next;
+        }
+        current = head;
+        while (current != null) {
+            Node next = current.next;
+            Node random = current.random;
+
+            Node copy = map.get(current);
+            copy.next = map.get(next);
+            copy.random = map.get(random);
+            current = current.next;
+        }
+        return map.get(head);
+    }
+}
+
