edfde86aae7b0b159de496d01a926240bcaff4ca
mo6044-hash <mo6044@princeton.edu>
2026-06-25 00:45:51 -0400

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (fast != null && slow != null) {
+            slow = slow.next;
+            if (fast.next == null) return false;
+            fast = fast.next.next;
+
+            if (fast == slow) return true;
+        }
+        return false;
+
+    }
+}
