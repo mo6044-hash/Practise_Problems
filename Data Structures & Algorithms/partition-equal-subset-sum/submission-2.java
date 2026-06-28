ebf87fda4ff191f8530857bb7839cb99c012399f
mo6044-hash <mo6044@princeton.edu>
2026-06-28 15:54:58 -0400

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    private Boolean[][] memo;
+    private boolean dfs(int index, int remaining, int[] nums) {
+        if (index == nums.length || remaining < 0) return false;
+        if (remaining == 0) {
+            memo[index][remaining] = true;
+            return true;
+        }
+
+        if (memo[index][remaining] != null) {
+            return memo[index+1][remaining];
+        } else {
+            memo[index+1][remaining] = (dfs(index+1, remaining-nums[index], nums) || dfs(index+1, remaining, nums));
+            return memo[index+1][remaining];
+        }
+
+
+    }
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) return false;
+        int remaining = sum / 2;
+        memo = new Boolean[remaining+1][remaining+1];
+        return dfs(0, remaining, nums);
+    }
+}
